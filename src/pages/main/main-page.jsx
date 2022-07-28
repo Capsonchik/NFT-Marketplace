5b7bdaef63c719112520f71190c53dec29d7cbe0
Capsonchik <Serega00790@mail.ru>
2022-07-28 14:03:14 +0300

    new economy block

--- src/pages/main/main-page.jsx
+++ src/pages/main/main-page.jsx
@@ -1,10 +1,12 @@
 import EarnFreeCrypto from "../../components/earnFreeCrypto/earn-free-crypto";
 import PopularSellers from "../../components/popularSellers/popular-sellers";
+import NewEconomyBlock from "../../components/newCreativeEconomy/new-creative-economy";
 
 
 export const MainPage = ({users}) => {
     return(
         <div className="main">
+            <NewEconomyBlock/>
             <PopularSellers users={users}/>
             <EarnFreeCrypto/>
         </div>

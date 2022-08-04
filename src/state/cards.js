2f352dd2e21c6faf7e26dc1803abf66a30ba73bd
Cra7yy <cra7yy@gmail.com>
2022-08-04 11:14:10 +0300

    fix object

--- src/state/cards.js
+++ src/state/cards.js
@@ -5,7 +5,7 @@ export const Cards = [
         cardName: 'Top Cube',
         price: 2.05,
         value: 'ETH',
-        category: 'art',
+        category: 'Cubism',
         owner: "Marlee",
         description: 'Wonderful lines'
     },
@@ -15,7 +15,7 @@ export const Cards = [
         cardName: 'Night Ocean',
         price: 7.32,
         value: 'BTC',
-        category: 'music',
+        category: 'Landscape',
         owner: "Marlee",
         description: 'some text description'
     },
@@ -25,7 +25,7 @@ export const Cards = [
         cardName: 'Pretty woman',
         price: 4.45,
         value: 'ETH',
-        category: 'video',
+        category: 'Art',
         owner: "Marlee",
         description: 'some text description'
     },
@@ -35,7 +35,7 @@ export const Cards = [
         cardName: 'Wonderful lines',
         price: 15.015,
         value: 'ETH',
-        category: 'photography',
+        category: 'Geometric Abstractionism',
         owner: "Marlee",
         description: 'some text description'
     },
@@ -43,21 +43,190 @@ export const Cards = [
         id: 5,
         img: 'img/cards/img107.png',
         cardName: 'Amazing digital art',
-        price: 15.015,
+        price: 5.015,
         value: 'ETH',
-        category: 'music',
-        owner: "Marlee",
+        category: 'Metarealism',
+        owner: "Payton1",
         description: 'some text description'
     },
     {
         id: 6,
         img: 'img/cards/img108.png',
-        cardName: 'Amazing digital art',
-        price: 15.015,
+        cardName: 'Cloud cat',
+        price: 4.5,
         value: 'ETH',
-        category: 'game',
-        owner: 23262,
+        category: 'Art',
+        owner: 'Edd',
         description: 'some text description'
     },
-
+    {
+        id: 7,
+        img: 'img/cards/img109.png',
+        cardName: 'Woman in the clouds',
+        price: 6.03,
+        value: 'ETH',
+        category: 'Art',
+        owner: 'Edd',
+        description: 'some text description'
+    },
+    {
+        id: 8,
+        img: 'img/cards/img110.png',
+        cardName: 'Unexpected woman',
+        price: 8.45,
+        value: 'ETH',
+        category: 'Pop Art',
+        owner: 'Edd',
+        description: 'some text description'
+    },
+    {
+        id: 9,
+        img: 'img/cards/img113.png',
+        cardName: 'Alight lake',
+        price: 7.13,
+        value: 'ETH',
+        category: 'Art',
+        owner: 'Edd',
+        description: 'some text description'
+    },
+    {
+        id: 10,
+        img: 'img/cards/img114.png',
+        cardName: 'Mirror triangle',
+        price: 2.89,
+        value: 'ETH',
+        category: 'Cubism',
+        owner: "Odell",
+        description: 'some text description'
+    },
+    {
+        id: 11,
+        img: 'img/cards/img115.png',
+        cardName: 'Sorceress mask',
+        price: 3.003,
+        value: 'ETH',
+        category: 'Photo',
+        owner: "Odell",
+        description: 'some text description'
+    },
+    {
+        id: 12,
+        img: 'img/cards/img116.png',
+        cardName: 'Mysterious woman ',
+        price: 5.095,
+        value: 'ETH',
+        category: 'Art',
+        owner: "Odell",
+        description: 'some text description'
+    },
+    {
+        id: 13,
+        img: 'img/cards/img119.png',
+        cardName: 'Black hole',
+        price: 9.017,
+        value: 'ETH',
+        category: 'Cosmism',
+        owner: "Odell",
+        description: 'some text description'
+    },
+    {
+        id: 14,
+        img: 'img/cards/img120.png',
+        cardName: 'Beloved child',
+        price: 7.015,
+        value: 'ETH',
+        category: 'Painting',
+        owner: "Payton1",
+        description: 'some text description'
+    },
+    {
+        id: 15,
+        img: 'img/cards/img121.png',
+        cardName: 'Man in the paint',
+        price: 5.025,
+        value: 'ETH',
+        category: 'Art',
+        owner: "Payton",
+        description: 'some text description'
+    },
+    {
+        id: 16,
+        img: 'img/cards/img122.png',
+        cardName: 'Propaganda',
+        price: 3.035,
+        value: 'ETH',
+        category: 'Art',
+        owner: "Payton",
+        description: 'some text description'
+    },
+    {
+        id: 17,
+        img: 'img/cards/img125.png',
+        cardName: 'Duriman',
+        price: 7.045,
+        value: 'ETH',
+        category: 'Photo',
+        owner: "Payton",
+        description: 'some text description'
+    },
+    {
+        id: 18,
+        img: 'img/cards/img126.png',
+        cardName: 'Fiery fox',
+        price: 6.5,
+        value: 'ETH',
+        category: 'Art',
+        owner: "Payton",
+        description: 'some text description'
+    },
+    {
+        id: 19,
+        img: 'img/cards/img127.png',
+        cardName: 'Northern lights',
+        price: 4.15,
+        value: 'ETH',
+        category: 'Actionism',
+        owner: "Payton1",
+        description: 'some text description'
+    },
+    {
+        id: 20,
+        img: 'img/cards/img128.png',
+        cardName: 'Rainbow corner',
+        price: 3.011,
+        value: 'ETH',
+        category: 'Wonderful lines',
+        owner: "Payton1",
+        description: 'some text description'
+    },
+    {
+        id: 21,
+        img: 'img/cards/img131.png',
+        cardName: 'Around the corner',
+        price: 4.415,
+        value: 'ETH',
+        category: 'Art',
+        owner: "Edd1",
+        description: 'some text description'
+    },
+    {
+        id: 22,
+        img: 'img/cards/img133.png',
+        cardName: 'American girl',
+        price: 3.813,
+        value: 'ETH',
+        category: 'Comics',
+        owner: "Edd1",
+        description: 'some text description'
+    },
+    {
+        id: 23,
+        img: 'img/cards/img134.png',
+        cardName: 'Flying cactus',
+        price: 7.075,
+        value: 'ETH',
+        category: 'Art',
+        owner: "Edd1",
+        description: 'some text description'
+    },
 ]
--- src/state/users.js
+++ src/state/users.js
@@ -2,7 +2,7 @@ export const Users = [
     {
         "id": 1,
         "nickName": "Edd",
-        "photo": 'img/avatars/edd-harrys.png',
+        "photo": "img/avatars/edd-harrys.png",
         "name": "Edd Harrys",
         "description": "A wholesome farm owner in Montana. Upcoming gallery solo show in Germany",
         "balance": 27.45,
@@ -19,21 +19,8 @@ export const Users = [
         "achivments": "#3772FF",
         "collections": [
             {
-                collectionName: "Edwards Collection",
-                cardsCollection: [
-                    {
-                        id: 1
-                    },
-                    {
-                        id: 2
-                    },
-                    {
-                        id: 3
-                    },
-                    {
-                        id: 4
-                    }
-                ]
+                collectionName: "Edward Collection",
+                cardsCollection: [6,7,8,9]
             }
         ]
     },
@@ -45,8 +32,21 @@ export const Users = [
         "description": "some words",
         "balance": 3.22,
         "cards": [],
-        "social": [],
-        "achivments": "#45B36B"
+        "social": [
+            {
+                "twitter": "https://twitter.com/?lang=ru",
+                "instagram": "https://www.instagram.com/",
+                "facebook": "https://facebook.com",
+            }
+        ],
+        "registerDate": "Apr 5,2021",
+        "achivments": "#45B36B",
+        "collections": [
+            {
+                collectionName: "Odell Collection",
+                cardsCollection: [10,11,12,13]
+            }
+        ]
     },
     {
         "id": 3,
@@ -56,8 +56,21 @@ export const Users = [
         "description": "some words",
         "balance": 8.41,
         "cards": [],
-        "social": [],
-        "achivments": "#3772FF"
+        "social": [
+            {
+                "twitter": "https://twitter.com/?lang=ru",
+                "instagram": "https://www.instagram.com/",
+                "facebook": "https://facebook.com",
+            }
+        ],
+        "registerDate": "Mai 6,2021",
+        "achivments": "#3772FF",
+        "collections": [
+            {
+                collectionName: "Marlee Collection",
+                cardsCollection: [1,2,3,4]
+            }
+        ]
     },
     {
         "id": 4,
@@ -67,29 +80,68 @@ export const Users = [
         "description": "some words",
         "balance": 100.94,
         "cards": [],
-        "social": [],
-        "achivments": "#23262F"
+        "social": [
+            {
+                "twitter": "https://twitter.com/?lang=ru",
+                "instagram": "https://www.instagram.com/",
+                "facebook": "https://facebook.com",
+            }
+        ],
+        "registerDate": "Mar 25,20220",
+        "achivments": "#23262F",
+        "collections": [
+            {
+                collectionName: "Payton Collection",
+                cardsCollection: [15,16,17,18]
+            }
+        ]
     },
     {
         "id": 5,
-        "nickName": "Payton",
+        "nickName": "Payton1",
         "photo": 'img/avatars/payton-buckridge.png',
         "name": "Payton Buckridge",
         "description": "some words",
         "balance": 35.67,
         "cards": [],
-        "social": [],
-        "achivments": "#777E91"
+        "social": [
+            {
+                "twitter": "https://twitter.com/?lang=ru",
+                "instagram": "https://www.instagram.com/",
+                "facebook": "https://facebook.com",
+            }
+        ],
+        "registerDate": "Mar 17,2021",
+        "achivments": "#777E91",
+        "collections": [
+            {
+                collectionName: "Payton Collection",
+                cardsCollection: [5,14,19,20]
+            }
+        ]
     },
     {
         "id": 6,
-        "nickName": "Edd",
+        "nickName": "Edd1",
         "photo": 'img/avatars/edd-harrys.png',
         "name": "Edd Harrys",
         "description": "some words",
         "balance": 7.45,
         "cards": [],
-        "social": [],
-        "achivments": "#3772FF"
+        "social": [
+            {
+                "twitter": "https://twitter.com/?lang=ru",
+                "instagram": "https://www.instagram.com/",
+                "facebook": "https://facebook.com",
+            }
+        ],
+        "registerDate": "Mar 23,2021",
+        "achivments": "#3772FF",
+        "collections": [
+            {
+                collectionName: "Edd Collection",
+                cardsCollection: [21,22,23]
+            }
+        ]
     }
 ]
